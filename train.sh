d11c181a3d5e964a1ae778206b5fcaf7cd2a44ff
Getachew Workineh Gella <82418348+getch-geohum@users.noreply.github.com>
2023-07-03 14:36:16 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #for data in 1 2  4 5
 #do
 python InceptionTimeFuse.py\
-	--data_root=/home/getch/ssl/EO_Africa/samples\
-    	--weight_path=/home/getch/ssl/EO_Africa/MODELS\
-    	--log_path=/home/getch/ssl/EO_Africa/LOGS\
-    	--index_fold=/home/getch/ssl/EO_Africa/FILL\
-    	--pred_scene_fold=/home/getch/ssl/EO_Africa/PREDS\
-    	--scene_fold=/home/getch/ssl/EO_Africa/FILL\
-	--feature_fold=/home/getch/ssl/EO_Africa/FEATURES\
-    	--raster_tempelate=/home/getch/ssl/EO_Africa/plntMask.tif\
+	--data_root=./EO_Africa/samples\
+    	--weight_path=./EO_Africa/MODELS\
+    	--log_path=./EO_Africa/LOGS\
+    	--index_fold=./EO_Africa/FILL\
+    	--pred_scene_fold=./EO_Africa/PREDS\
+    	--scene_fold=./EO_Africa/FILL\
+	--feature_fold=./EO_Africa/FEATURES\
+    	--raster_tempelate=./EO_Africa/plntMask.tif\
     	--hdims=128\
     	--nlayer=4\
 	--dropout=0.15\
